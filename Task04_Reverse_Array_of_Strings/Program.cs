2dbd757a28861fd36f9b0cff38d5c316755b4491
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:02:49 +0200

    Add files via upload

---
+++ Task04_Reverse_Array_of_Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Task04_Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = item.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{item[i]} ");
+            }
+        }
+    }
+}
