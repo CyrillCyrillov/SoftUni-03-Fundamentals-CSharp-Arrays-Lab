c2f578695f57e2d45b6c05ec865769d3f5ab7fd9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:05:46 +0200

    Add files via upload

---
+++ Task08_Condense_Array_to_Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Task08_Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int length = numbers.Length;
+
+            while(length > 1)
+            {
+                int[] condensed = new int[length - 1];
+                for (int i = 0; i < length - 1; i++)
+                    {
+                        condensed[i] = numbers[i] + numbers[i + 1];
+                    }
+                for (int i = 0; i < length - 1; i++)
+                {
+                    numbers[i] = condensed[i];
+                }
+
+                length -= 1;
+            }
+
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
